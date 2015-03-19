2cd4242e3727c7b9b83e82232d70273200b9f419
Tomasz Stęczniewski <tomaszsteczniewski@gmail.com>
2015-03-19 20:25:15 +0100

    Początek prac nad rnn.

---
+++ deeplearning4j-scaleout/spark/dl4j-spark-nlp/src/main/java/org/deeplearning4j/spark/models/rnn/Leaf.java
@@ -0,0 +1,5 @@
+package org.deeplearning4j.spark.models.rnn;
+
+public class Leaf<A> extends Tree<A> {
+    public A value;
+}
---
+++ deeplearning4j-scaleout/spark/dl4j-spark-nlp/src/main/java/org/deeplearning4j/spark/models/rnn/Node.java
@@ -0,0 +1,7 @@
+package org.deeplearning4j.spark.models.rnn;
+
+public class Node<A> extends Tree<A> {
+    public Tree<A> left;
+    public Tree<A> right;
+}
+
